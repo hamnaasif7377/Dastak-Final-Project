d817787e1083ac487b6e58f7eb652b94b39f61f8
Hamna Asif <hamnaasif7377@gmail.com>
2025-12-02 08:57:52 +0500

    Initial Commit

---
+++ app/src/test/java/com/DASTAK/i230613_i230658_i230736/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.DASTAK.i230613_i230658_i230736
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "i230613_i230658_i230736"
+include(":app")
+ 
