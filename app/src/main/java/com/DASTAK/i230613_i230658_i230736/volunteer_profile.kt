ed32c7e5084d9940beafc260e44dde1f8b3e9ea1
i230613-max <i230613@isb.nu.edu.pk>
2025-12-06 19:30:39 +0500

    x2

---
+++ app/src/main/java/com/DASTAK/i230613_i230658_i230736/volunteer_profile.kt
@@ -0,0 +1,20 @@
+package com.DASTAK.i230613_i230658_i230736
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class volunteer_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_volunteer_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
